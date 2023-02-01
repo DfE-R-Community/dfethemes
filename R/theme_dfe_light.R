77d2f8038952bc673a82f44ab3592560461386c4
Sam <sam.cooke@education.gov.uk>
2023-02-01 23:29:29 +0000

    Feat / Docs - altering theme, adding a plot to readme

--- R/theme_dfe_light.R
+++ R/theme_dfe_light.R
@@ -20,20 +20,27 @@ theme_dfe_light <- function() {
     text = element_text(colour = "#4d4d4d", size = 14),
 
 
-    axis.title.y = element_text(hjust = 1, angle = 90),
+    axis.title.y = element_text(hjust = 0, angle = 90),
     axis.title.x = element_text(hjust = 0, angle = 0),
     axis.text = element_text(colour = "#4d4d4d"),
     axis.ticks = element_line(colour = "#b3b3b3"),
     axis.line = element_line(colour = "#b3b3b3"),
+    axis.line.y = element_blank(),
+    axis.line.x = element_line(lineend = "square", linewidth = 1),
 
     panel.background = element_rect(fill = "#FFFFFF"),
     panel.grid = element_blank(),
     panel.grid.major.y = element_line(colour = "#d9d9d9"),
     panel.grid.minor.y = element_line(colour = "#e6e6e6"),
 
+    strip.background = element_rect(fill = "#FFFFFF",
+                                    colour = "#b3b3b3", linewidth = 1),
+
 
     plot.title = element_text(face = "bold", colour = dfe_colours["Blue"]),
     legend.position = "top",
-    legend.key = element_rect(fill = "#FFFFFF")
+    legend.key = element_rect(fill = "#FFFFFF"),
+    plot.margin = margin(5,4,4,3)
         )
 }
+

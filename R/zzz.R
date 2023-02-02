664da93dd5d79cae00c2ea7140b93c095634e71f
Sam <sam.cooke@education.gov.uk>
2023-02-02 00:06:01 +0000

    Feat - adding in default loading of themes/scales

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+.onLoad <- function(libname, pkgname){
+  cli_alert_info("setting default ggplot2 theme to {.val theme_nimbus()}")
+  theme_set(theme_nimbus())
+
+  cli_alert_info("Setting default ggplot2 colour scales to {.val scale_colour_dfe()}")
+
+  options(
+   ggplot2.continuous.colour = function() scale_colour_dfe(palette = "cool", discrete = FALSE),
+   ggplot2.discrete.colour   = function() scale_colour_dfe(),
+   ggplot2.continuous.fill   = function() scale_fill_dfe(palette = "cool", discrete = FALSE),
+   ggplot2.discrete.fill     = function() scale_fill_dfe()
+  )
+}
